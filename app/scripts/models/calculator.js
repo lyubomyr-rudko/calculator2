1c5bc9be302868ce80edf3de25959325d307aafa
Lyubomyr Rudko <lrudko@lohika.com>
2013-12-04 06:49:31 +0200

    Refactoring

--- app/scripts/models/calculator.js
+++ app/scripts/models/calculator.js
@@ -7,52 +7,62 @@ define([
     'use strict';
 
     var CalculatorModel = Backbone.Model.extend({
-        params: [],
+        params: null,
+        currentParam: null,
         action: null,
 
+        initialize: function () {
+            this.reset();
+        },
+
         addValue: function (val) {
-            var currentParam;
-
-            if (!this.params.length) {
-                this.params.push([]);
-            }
-
-            currentParam = this.params[this.params.length - 1];
-
-            if (currentParam instanceof Array) {
-                if (val === '.' && this.dotIsEntered(currentParam)) {
+            //check if we nead to add a value to this param, or replace it
+            if (this.currentParam instanceof Array) {
+                //dot is allowed only once
+                if (val === '.' && this.dotIsEntered(this.currentParam)) {
                     return;
                 }
-
-                currentParam.push(val);
             } else {
                 if (val === '.') {
                     val = '0.';
                 }
 
+                this.params.second = [];
                 if (this.action) {
-                    this.params.push([val]);
+                    this.currentParam = this.params.second;
+                    //this.params.push([val]);
                 } else {
-                    this.params = [[val]];
+                    this.params.first = [];
+                    this.currentParam = this.params.first;
+                    // this.params = [[val]];
                 }
             }
+
+            this.currentParam.push(val);
         },
 
         dotIsEntered: function (arr) {
             return _.some(arr, function (item) { return '.' === item; });
         },
 
+        reset: function () {
+            this.params = {first: [], second: []};
+            this.currentParam = this.params.first;
+            this.action = null;
+        },
+
         addAction: function (a) {
             if (a === this.actions.ce) {
-                this.params = [];
-                this.action = null;
+                this.reset();
                 return;
             }
 
-            if (this.params.length === 2) {
+            if (this.params.second === this.currentParam) {
                 this.performAction();
-            } else if (a !== this.actions.equal) {
-                this.params.push([]);
+            }
+
+            if (a !== this.actions.equal) {
+                this.currentParam = this.params.second;
             }
 
             this.action = a;
@@ -61,15 +71,16 @@ define([
                 this.action = null;
             }
         },
+
         performAction: function () {
             var value1,
-                value2 = parseFloat(this.params[1].join('')),
+                value2 = parseFloat(this.params.second.join('')),
                 res = 0;
 
-            if (this.params[0] instanceof Array) {
-                value1 = parseFloat(this.params[0].join(''));
+            if (this.params.first instanceof Array) {
+                value1 = parseFloat(this.params.first.join(''));
             } else {
-                value1 = this.params[0];
+                value1 = this.params.first;
             }
 
             if (this.action === this.actions.add) {
@@ -82,21 +93,24 @@ define([
                 res = value1 / value2;
             }
 
-            this.params = [res];
+            this.reset();
+
+            this.params.first = res;
+            this.currentParam = this.params.first;
         },
 
         getOutput: function () {
             var value1,
                 value2;
 
-            if (this.params[1]) {
-                value2 = this.params[1].join('');
+            if (this.params.second) {
+                value2 = this.params.second.join('');
             }
 
-            if (this.params[0] instanceof Array) {
-                value1 = this.params[0].join('');
+            if (this.params.first instanceof Array) {
+                value1 = this.params.first.join('');
             } else {
-                value1 = this.params[0];
+                value1 = this.params.first;
             }
 
             return value2 || value1 || '0';
